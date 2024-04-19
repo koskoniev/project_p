8d198406dcba58f1111644e51a0cc57aa7e0e953
koskoniev <koskoniev@gmail.com>
2024-04-19 17:01:48 +0300

    some stages

--- main.py
+++ main.py
@@ -1,55 +1,65 @@
-# class Lite:
-#     def __init__(self):
-#         self.lite = False
+# from random import 
+import uuid
 
-#     def status(self):
-#         print("Light", self.lite)
-#         return self.lite
+class Man:
+    def __init__(self, name):
+        self.name = name
+        self.id = str(uuid.uuid4())[:8]
 
-#     def switch(self):
-#         if self.lite:
-#             self.lite = False
-#             return True
-#         else:
-#             self.lite = True
-#             return True
+class Pers(Man):
+    def __init__(self, name, unit="", level=0):
+        super().__init__(name)
+        self.unit = unit
+        self.level = level
+    def get_level(self):
+        return self.level
+    def level_up(self):
+        self.level += 1
+    def level_down(self):
+        self.level -= 1
+    def __str__(self):
+        return f"{self.name} {self.unit}"
 
-#     def on(self):
-#         self.lite = True
-#         return True
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+    def add_gr(self, pers):
+        self.group.add(pers)
+    def get_gr(self):
+        t = ""
+        for i in self.group:
+            t += f"{i.id} {i.name} {i.level} {i.unit}\n"
+        return f"{self.number}: \n{t}"
+    def delete_gr(self, name):
+        # t = tuple(self.group)
+        # for i in t:
+        for i in tuple(self.group):
+            if i.name == name:
+                self.group.remove(i)
 
-#     def off(self):
-#         self.lite = False
-#         return True
+        # return self.group
+    def find_pers(self, name):
+        for i in self.group:
+            if i.name == name:
+                print(i.name, i.id)
+        
 
 
-# lite = Lite()
-# print(lite.status())
-# print(lite.on())
-# print(lite.status())
-# print(lite.switch())
-# print(lite.status())
-# lite.status()
-# lite.on()
-# lite.status()
-# lite.switch()
-# lite.status()
-
-l = list()
-if l:
-    print("zero")
-else:
-    print("else")
-l.append(1)
-# l.append(True)
-# l.append(False)
-# l.append("TEXT")
-# l.append('')
-# l.append(" ")
-print(bool(l))
-# for i in l:
-if l:
-    print("zero")
-else:
-    print("else")
-        # print(bool(l))
+a = Pers("a")
+a.unit = "Astronaut"
+b = Pers("b")
+b.unit = "Badboy"
+c = Pers("c")
+c.unit = "Caesar"
+d = Pers("d")
+# print(a)
+g = Group("G_")
+g.add_gr(a)
+g.add_gr(b)
+g.add_gr(c)
+g.add_gr(d)
+print(g.get_gr())
+# g.find_pers("a")
+g.delete_gr('a')
+print(g.get_gr())
